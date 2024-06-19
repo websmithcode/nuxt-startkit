5e5ce154f7c099a1af1e3c7728f373f73dbb9a0a
websmithcode <websmithcode@gmail.com>
2024-06-20 02:42:54 +0300

    Add ui with deps

---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex flex-col justify-center">
+
+    <h1 class="text-3xl font-bold text-center mb-3">Hello world!</h1>
+    <NuxtLink to="/demo" class="button success mx-auto">Go to demo</NuxtLink>
+  </div>
+</template>
