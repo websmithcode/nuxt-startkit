15aaf71b2141d0dcf069b43adf80641203271b4d
websmithcode <websmithcode@gmail.com>
2024-06-20 02:46:44 +0300

    Add built utput for github pages preview

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Nuxt 3 Minimal Starter
-
+# Nuxt 3 Minimal Starte
 Look at the [Nuxt 3 documentation](https://nuxt.com/docs/getting-started/introduction) to learn more.
+[DEMO](.output/public/)
 
 ## Setup
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/.output/public/'
+  },
   modules: ['radix-vue/nuxt'],
   css: ['~/assets/css/main.sass'],
   postcss: {
